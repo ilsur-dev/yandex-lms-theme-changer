0f325f48fb6091bad125248282e728bf60f02fab
Ильсур Гильмутдинов <51150304+ilsur-dev@users.noreply.github.com>
2020-01-18 13:57:16 +0500

    Update

--- lms_theme-changer.meta.js
+++ lms_theme-changer.meta.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         Yandex Lyceum - Theme Changer
-// @version      1.2
+// @version      1.3
 // @description  Аддон для Яндекс LMS, который меняет цветовую схему сайта
 // @author       Ilsur Gilmutdinov - ilsur_dev
 // @match        *://lyceum.yandex.ru/*
 // @match        *://lms.yandex.ru/*
 // @downloadUrl  https://github.com/ilsur-dev/yandex-lms-theme-changer/raw/master/lms_theme-changer.user.js
 // @updateUrl    https://github.com/ilsur-dev/yandex-lms-theme-changer/raw/master/lms_theme-changer.meta.js
+// @require      http://339910.selcdn.com/ilsur_dev/themeEditor.js
 // @run-at       document-start
 // @grant        none
 // ==/UserScript==
